41c7163dc306e649667a87e6af54c9eeffe19125
SasiN <105596572+SasiNKeiko@users.noreply.github.com>
2022-11-17 17:58:12 +0100

    PauseMenu is working

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -8,16 +8,22 @@ public class PauseMenu : MonoBehaviour
 
     [SerializeField] GameObject pauseMenu;
 
+    public GameObject gameManager;
+
     public void Pause()
     {
         pauseMenu.SetActive(true);
         Time.timeScale = 0f;
+
+        gameManager.SetActive(false);
     }
 
     public void Resume()
     {
         pauseMenu.SetActive(false);
         Time.timeScale = 1f;
+
+        gameManager.SetActive(true);
     }
 
 }
