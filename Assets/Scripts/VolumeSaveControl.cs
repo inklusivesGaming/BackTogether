575cffcf065fe6bf4f14bad0dc4c03300c16e812
heroesLeft <stewi98@googlemail.com>
2022-11-19 13:59:48 +0100

    Fix volume control

--- Assets/Scripts/VolumeSaveControl.cs
+++ Assets/Scripts/VolumeSaveControl.cs
@@ -6,11 +6,11 @@ using UnityEngine.UI;
 
 public class VolumeSaveControl : MonoBehaviour
 {
+    public static bool mFirstLoad = true;
 
     [SerializeField] private Slider volumeSlider = null;
 
     [SerializeField] private TextMeshProUGUI volumeTextUI = null;
-    private float volumeValue;
 
     private void Start()
     {
@@ -24,15 +24,21 @@ public class VolumeSaveControl : MonoBehaviour
 
     public void SaveVolumeButton()
     {
-        volumeValue = volumeSlider.value;
+        float volumeValue = volumeSlider.value;
         PlayerPrefs.SetFloat("VolumeValue", volumeValue);
         LoadValues();
     }
 
     void LoadValues()
     {
-        float volumeValue = PlayerPrefs.GetFloat("VolumeValue");
-        print(volumeValue);
+        float volumeValue = 1f;
+        if (!mFirstLoad)
+            volumeValue = PlayerPrefs.GetFloat("VolumeValue");
+        else
+        {
+            PlayerPrefs.SetFloat("VolumeValue", volumeValue);
+            mFirstLoad = false;
+        }
         volumeSlider.value = volumeValue;
         AudioListener.volume = volumeValue;
     }
