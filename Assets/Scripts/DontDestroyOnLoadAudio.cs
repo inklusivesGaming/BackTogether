b0f52503df1a670928eb5bc2e23a63fb0a5caa97
heroesLeft <stewi98@googlemail.com>
2022-11-19 12:20:54 +0100

    Fix menu sound

--- Assets/Scripts/DontDestroyOnLoadAudio.cs
+++ Assets/Scripts/DontDestroyOnLoadAudio.cs
@@ -2,11 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DontDestroyOnLoadScript : MonoBehaviour
+public class DontDestroyOnLoadAudio : MonoBehaviour
 {
+    public static bool mAlreadyExists = false;
+
     // Start is called before the first frame update
     void Start()
     {
+        if (mAlreadyExists)
+            Destroy(gameObject);
+        mAlreadyExists = true;
         DontDestroyOnLoad(gameObject);
     }
 
