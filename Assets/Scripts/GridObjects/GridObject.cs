35f8e234ea703754ea30419634dda4aad690c95e
heroesLeft <stewi98@googlemail.com>
2023-01-04 18:03:46 +0100

    Add hole model

--- Assets/Scripts/GridObjects/GridObject.cs
+++ Assets/Scripts/GridObjects/GridObject.cs
@@ -5,34 +5,7 @@ using UnityEngine.VFX;
 
 public class GridObject : MonoBehaviour
 {
-
-    public MeshRenderer mMeshRenderer;
     public VisualEffect mPoufEffect;
-    
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    // Is called when the object gets selected
-    public virtual void OnSelected()
-    {
-        mMeshRenderer.material.color = Color.yellow;
-    }
-
-    // Is called when object gets deselected
-    public virtual void OnDeselected()
-    {
-        mMeshRenderer.material.color = Color.white;
-    }
 
     public virtual void Pouf()
     {
